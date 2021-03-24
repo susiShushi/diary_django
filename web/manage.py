5eb3d3fe2e047a23d175cc5fec9dd39ef6064a65
susiShushi <sushishushi760@gmail.com>
2021-03-24 22:49:07 +0900

    inquiry backend

--- web/manage.py
+++ web/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'privatediary.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'privatediary.settings_dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
